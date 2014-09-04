66125421caeac7122268dc2c20e65c457b48f1f2
andrewbass <thefuzz007@yahoo.com>
2014-09-04 15:01:41 -0400

    Added new class in RaytracerTest to store test scenes.
    Will help when more shapes are added.

--- Raytracer/src/Raytracer/Materials/Gradient.java
+++ Raytracer/src/Raytracer/Materials/Gradient.java
@@ -1,15 +1,23 @@
 package Raytracer.Materials;
 
+import Raytracer.Debugging.Debug;
 import Raytracer.Math.Color;
 import Raytracer.Math.Vec3;
 
 public class Gradient extends Material {
-	private Color from, to,diff;
+	public static final Gradient DAY_SKY = new Gradient(new Color(30,144,255),
+														new Color(135,206,250));
+	
+	public static final Gradient DUSK_SKY = new Gradient(new Color(15,45,180), 
+														 new Color(200,45,10));
+	
+	private Color from, to, diff;
 	
 	public Gradient(Color from, Color to){
 		super(0,0);
 		this.from = from;  this.to = to;
 		diff = to.sub(from);
+		Debug.write(diff);
 	}
 
 	public Color getAmbient(Vec3 point) {
@@ -18,7 +26,7 @@ public class Gradient extends Material {
 
 	public Color getDiffuse(Vec3 point) {
 		
-		return from.add(diff.scale(point.y));
+		return from.add(diff.scale(1-point.y));
 	}
 
 	public Color getSpecular(Vec3 point) {
--- RaytracerTest/src/Program.java
+++ RaytracerTest/src/Program.java
@@ -9,34 +9,18 @@ import Raytracer.Sampling.*;
 import Raytracer.Swing.*;
 
 public class Program {
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args){
 
 		JRaytracer raytracer = new JRaytracer("Raytracer (Alt + Enter to fullscreen)",
 											 1280, 720,	
-											 new Phong(FlatColor.BLACK),
-											 new Poseidon(4, 0.25));
+											 new Phong(Gradient.DAY_SKY),
+											 new Poseidon(9, 0.2));
 
 		Camera cam = new Camera(new Vec3(0,7,-5), Vec3.FORWARD, Vec3.UP, 60);
 		
-		// Material
-		Checkered reflectiveSurface = new Checkered(1000, Color.WHITE, Color.DARK_BLUE);
-		reflectiveSurface.setReflectivity(5);
-		
-		// Geometry
-		Sphere sphere 	= new Sphere(new Vec3(0,7,20), 7, Checkered.WHITE_RED);
-		Sphere sphere2 	= new Sphere(new Vec3(-20, 5, 30), 5, FlatColor.MAGNETA);
-		Plane plane 	= new Plane(Vec3.ZERO, Vec3.UP, reflectiveSurface);
-
-		// Lights
-		PointLight light 	= new PointLight(new Vec3(0,50,0), Color.WHITE);
-		PointLight light2	= new PointLight(new Vec3(0,20,-50), Color.WHITE);
-	
-		Scene scene = new Scene();
-		scene.addGeometry(sphere, sphere2, plane);
-		scene.addLight(light, light2);
 
 		Debug.LOG.start("Test render");
-		raytracer.render(scene, cam);
+		raytracer.render(ExampleScenes.TWO_SPHERES_PLANE, cam);
 		Debug.LOG.end();
 		
 		
